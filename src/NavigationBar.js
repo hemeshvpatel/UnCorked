5ecbc7b17b0cf3519aab044ae0e8c92c3c420f16
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-01-18 18:05:12 -0600

    updated icon, added navbar notes

---
+++ src/NavigationBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Navbar } from "react-bootstrap/Navbar";
+
+class NavigationBar extends React.Component {
+  render() {
+    return <Navbar>UnCorked</Navbar>;
+  }
+}
+
+export default NavigationBar;
